94ee0d47319cd56f8b91d51ae8ae589ac8ae018c
의진 <128464842+eejx0@users.noreply.github.com>
2024-04-04 17:13:15 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 3.89 ms, Memory: 42.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    return arr.map(
+        a => {
+            if (a>=50 && a%2===0) {
+                return a/2
+            } else if (a<50 && a%2!==0) {
+                return a*2
+            }
+            return a;
+        }
+    );
+}
